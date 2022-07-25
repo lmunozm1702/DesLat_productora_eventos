64048d7197dda581ca4299a76b9c5099f58fbe4d
lmunozm1702 <53025062+lmunozm1702@users.noreply.github.com>
2022-07-25 17:26:46 -0400

    +all required reqs done 1 to 7

--- app/models/band.rb
+++ app/models/band.rb
@@ -3,11 +3,31 @@ class Band < ApplicationRecord
 
     enum band_type: %i[band girl_band boy_band]
 
+    def all_assistants
+        self.concerts.sum(:assistants)
+    end
+
     def this_month_concerts
         self.concerts.where(date: (Date.today.beginning_of_month)..(Date.today)).size
     end
 
     def last_concert
         self.concerts.order(:date).last
+    end
+
+    def last_concert_date
+        self.last_concert.date.strftime("%Y - %B - %d")
+    end
+
+    def most_popular_concert
+        self.concerts.maximum(:assistants)
+    end
+
+    def longest_concert
+        self.concerts.maximum(:duration)
+    end
+
+    def longest_concerts_location
+        self.concerts.where(duration: longest_concert)
     end
 end
